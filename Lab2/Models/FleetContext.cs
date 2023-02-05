c7ca85a331484f4f01618e1ee83ea76b90aa8c42
Ray Kazuya Murphy <85945564+Rayezor@users.noreply.github.com>
2023-02-05 20:49:23 +0000

    Updated Server info

--- Lab2/Models/FleetContext.cs
+++ Lab2/Models/FleetContext.cs
@@ -9,7 +9,7 @@ namespace Lab2.Models
 {
 	public class FleetContext : DbContext
 	{
-		private const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=EFCore;Trusted_Connection=False;";
+		private const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=EFCore1;Trusted_Connection=False;";
 		public DbSet<Car> cars { get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
